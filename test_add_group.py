96f283ed2112885081c16f1dcb58b1acba13a341
Thomas-IX <igorqa.9@gmail.com>
2016-01-27 00:06:48 +0400

    Refactoring

--- test_add_group.py
+++ test_add_group.py
@@ -1,49 +1,66 @@
-# -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+from selenium import WebDriver
+import unittest
+from group import Group
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-
-class test_add_group(unittest.TestCase):
+class Add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-    
-    def test_test_add_group(self):
-        success = True
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("asd")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("qwe")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("zxc")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
+        self.driver = WebDriver()
+        self.driver.implicitly_wait(60)
+
+    def open_home_page(self, driver):
+        driver.get("http://localhost/addressbook/")
+
+    def login(self, driver, username, password):
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys(username)
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_groups_page(self, driver):
+        driver.find_element_by_link_text("groups").click()
+
+    def create_group(self, driver, group):
+        driver.find_element_by_name("new").click()
+        driver.find_element_by_name("group_name").click()
+        driver.find_element_by_name("group_name").clear()
+        driver.find_element_by_name("group_name").send_keys(group.name)
+        driver.find_element_by_name("group_header").click()
+        driver.find_element_by_name("group_header").clear()
+        driver.find_element_by_name("group_header").send_keys(group.header)
+        driver.find_element_by_name("group_footer").click()
+        driver.find_element_by_name("group_footer").clear()
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
+        driver.find_element_by_name("submit").click()
+
+    def return_to_groups_page(self, driver):
+        driver.find_element_by_link_text("group page").click()
+
+    def logout(self, driver):
+        driver.find_element_by_link_text("Logout").click()
+
+    def test_add_group(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username = "admin", password = "secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, Group(name = "PyGroup", header = "Head", footer = "Foot"))
+        self.return_to_groups_page(driver)
+        self.logout(driver)
+
+    def test_add_emply_group(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username = "admin", password = "secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, Group(name = "", header = "", footer = ""))
+        self.return_to_groups_page(driver)
+        self.logout(driver)
+
     def tearDown(self):
-        self.wd.quit()
+        self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
