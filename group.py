33194f7cb79b8c8111b6f7f3ced6da18c7683fe9
Thomas <thomas@django.com>
2016-01-27 17:03:25 +0200

    Added a new class for future test

--- group.py
+++ group.py
@@ -1,6 +1,17 @@
 class Group:
 
-        def __init__(self, name, header, footer):
-            self.name = name
-            self.header = header
-            self.footer = footer
+    def __init__(self, name, header, footer):
+        self.name = name
+        self.header = header
+        self.footer = footer
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, address, mobile, email):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.address = address
+        self.mobile = mobile
+        self.email = email
