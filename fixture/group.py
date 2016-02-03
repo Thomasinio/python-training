29edbd48f5e8565cdccbbc11df9c88962ddf24e0
Thomas <thomas@django.com>
2016-02-03 08:59:56 +0200

    Added new method "test_del_group"

--- fixture/group.py
+++ fixture/group.py
@@ -3,7 +3,6 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
-
     def open_groups_page(self):
         driver = self.app.driver
         driver.find_element_by_link_text("groups").click()
@@ -22,7 +21,14 @@ class GroupHelper:
         driver.find_element_by_name("group_footer").clear()
         driver.find_element_by_name("group_footer").send_keys(group.footer)
         driver.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def delete_first_group(self):
+        driver = self.app.driver
         self.open_groups_page()
+        driver.find_element_by_name("selected[]").click()
+        driver.find_element_by_name("delete").click()
+        self.return_to_groups_page()
 
     def return_to_groups_page(self):
         driver = self.app.driver
