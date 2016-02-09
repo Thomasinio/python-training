8ad7fa9722036ae65d9f0b39f2be8a1fdc37d243
Thomas <thomas@django.com>
2016-02-09 13:52:33 +0200

    Changes in fixture

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,15 +1,6 @@
-import pytest
 from model.contact import Contact
-from fixture.application import Application
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_contact(app):
-    app.session.login(username = "admin", password = "secret")
     app.contact.create_contact(Contact(firstname = "Igor", middlename = "von", lastname = "V", nickname = "Thomas",
         address = "Blyuhera 7", mobile = "0936629380", email = "ivv@test.com"))
-    app.session.logout()
+
